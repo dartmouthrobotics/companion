1a3e4fe4fb00fd27f427eb6c7bc1df5cc8bdd94e
Alberto Quattrini Li <albertoq@cse.sc.edu>
2019-05-30 14:00:34 -0400

    Added options for gstreamer audio into a file, similarly to the video.

--- scripts/start_audio.sh
+++ scripts/start_audio.sh
@@ -1,6 +1,10 @@
 export LD_LIBRARY_PATH=/usr/local/lib/
+
+# load gstreamer options
+gstOptions=$(tr '\n' ' ' < $HOME/gstreamer2-audio.param)
+
 cd $HOME/
-gst-launch-1.0 -v -e alsasrc device=hw:1,0 ! audioconvert ! rtpL16pay ! udpsink host=192.168.2.1 port=5601
+gst-launch-1.0 -v -e alsasrc device=hw:1,0 $gstOptions
 
 
 #gst-launch-1.0 -v -e alsasrc device=hw:1,0 ! audioconvert ! tcpserversink host=192.168.2.2 port=5700
